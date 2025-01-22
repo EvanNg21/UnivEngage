084b27eed15c06266391f0576f89f1ce085d335c
Martin Ha <martindn.ha@gmail.com>
2025-01-22 00:49:20 -0800

    Modify cors.rb file to connect with React Client

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Where your requests come from, eg: your react app's IP address or URL.
-    origins "http://127.0.0.1:5173"
+    origins "http://localhost:5173"
 
     resource "*",
       headers: :any,
