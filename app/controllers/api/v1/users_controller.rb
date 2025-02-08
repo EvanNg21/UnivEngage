93699cbd1183493c2e7e5fa3fea8ba3171b2a59a
Evan <21ewng@gmail.com>
2025-02-07 16:34:45 -0800

    lint3

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,32 +5,32 @@ module Api
 
             def index
                 users = User.all
-                render json: { status: 'SUCCESS', users: users}, status: :ok
+                render json: { status: 'SUCCESS', users: users }, status: :ok
             end
 
             def show
-                render json: { status: 'SUCCESS', user: @user}, status: :ok
+                render json: { status: 'SUCCESS', user: @user }, status: :ok
             end
 
             def update
                 if @user.update(user_params)
-                    render json: { status: 'SUCCESS', user: @user}, status: :ok
+                    render json: { status: 'SUCCESS', user: @user }, status: :ok
                 else
-                    render json: { status: 'ERROR', user: @user.errors}, status: :unprocessable_entity
+                    render json: { status: 'ERROR', user: @user.errors }, status: :unprocessable_entity
                 end
             end
 
             def destroy
                 @user.destroy
-                render json: { status: 'SUCCESS', user: @user}, status: :ok
+                render json: { status: 'SUCCESS', user: @user }, status: :ok
             end
 
             def create # create user
                 user = User.new(user_params)
                 if user.save
-                    render json: { status: 'SUCCESS', user: user}, status: :created
+                    render json: { status: 'SUCCESS', user: user }, status: :created
                 else
-                    render json: { status: 'ERROR', user: user.errors}, status: :unprocessable_entity
+                    render json: { status: 'ERROR', user: user.errors }, status: :unprocessable_entity
                 end
             end 
 
