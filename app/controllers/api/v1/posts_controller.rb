1376db50188a3ef19164e8e1091db5cfba1ca90e
Martin Ha <martindn.ha@gmail.com>
2025-02-28 20:14:42 -0800

    Revert "posts error?"; Ignore errors in post api for now.
    This reverts commit e9801569e40ca34af5696b32604b6605280943f0.

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -42,8 +42,7 @@ class Api::V1::PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find_by(id: params[:id])
-      render json: { error: 'Post not found' }, status: :not_found unless @post
+      @post = Post.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
