d5eeb6403da6bcf97e8b4dbd09000c78e52202b9
Evan <21ewng@gmail.com>
2025-04-21 07:01:12 -0700

    update club if events or posts update

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-    belongs_to :club, foreign_key: :club_id
+    belongs_to :club, foreign_key: :club_id touch: true
     has_many :event_attendances
     has_many :users, through: :event_attendances, source: :user
 end
